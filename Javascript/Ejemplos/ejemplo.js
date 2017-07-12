64ee58234fed64d8fea3f826b4d632c302a213f1
Juliana Atehortua Quintero <andre.1999-10@hotmail.com>
2017-07-12 09:52:42 -0500

    ejemplo de javascript con alert y confirm

---
+++ Javascript/Ejemplos/ejemplo.js
@@ -0,0 +1,8 @@
+if(confirm("Estas contento en clase?")){
+	//Lo que se hace si se cumple la condicion
+	alert("Me caes bien");
+}
+else{
+	//Lo que se hace si No se cumple la condicion
+	alert("Mejor me quedo en la casa");
+}
